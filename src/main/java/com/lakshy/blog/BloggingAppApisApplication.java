a90c42ffafcff7915dca216536c47e95ac19745c
dylandsilva688 <66080245+dylandsilva688@users.noreply.github.com>
2025-10-09 18:27:34 +0530

    Update BloggingAppApisApplication.java

--- src/main/java/com/lakshy/blog/BloggingAppApisApplication.java
+++ src/main/java/com/lakshy/blog/BloggingAppApisApplication.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -15,52 +17,52 @@ import com.lakshy.blog.entities.Role;
 import com.lakshy.blog.repositories.RoleRepo;
 
 @SpringBootApplication
-public class BloggingAppApisApplication implements CommandLineRunner {
-	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+public class BloggingAppApisApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
-	@Autowired
-	private RoleRepo roleRepo;
-	
-	public static void main(String[] args) {
-		SpringApplication.run(BloggingAppApisApplication.class, args);
-	}
-	
-	@Bean
-	public ModelMapper modelMapper() 
-	{
-		return new ModelMapper();
-	}
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-	// will run automatically when main runs and passing arguments via terminal
-	@Override
-	public void run(String... args) throws Exception {
-		
-//		System.out.println(this.passwordEncoder.encode("12345"));
-		
-		// Creating 2 roles at the start of application
-		try {
-			
-			Role role = new Role();
-			role.setId(AppConstants.ADMIN_USER);
-			role.setName("ROLE_ADMIN");
-			
-			Role role1 = new Role();
-			role1.setId(AppConstants.NORMAL_USER);
-			role1.setName("ROLE_NORMAL");
-			
-			List<Role> roles = List.of(role,role1);
-			
-			List<Role> result = this.roleRepo.saveAll(roles);
-			
-			result.forEach(r -> {
-				System.out.println(r.getName());
-			});
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    @Autowired
+    private RoleRepo roleRepo;
 
+    public static void main(String[] args) {
+        SpringApplication.run(BloggingAppApisApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // This enables deployment to external Tomcat (WAR packaging)
+        return builder.sources(BloggingAppApisApplication.class);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Uncomment to print encoded password
+        // System.out.println(this.passwordEncoder.encode("12345"));
+
+        try {
+            // Create two roles: ADMIN and NORMAL
+            Role roleAdmin = new Role();
+            roleAdmin.setId(AppConstants.ADMIN_USER);
+            roleAdmin.setName("ROLE_ADMIN");
+
+            Role roleNormal = new Role();
+            roleNormal.setId(AppConstants.NORMAL_USER);
+            roleNormal.setName("ROLE_NORMAL");
+
+            List<Role> roles = List.of(roleAdmin, roleNormal);
+
+            List<Role> savedRoles = this.roleRepo.saveAll(roles);
+
+            savedRoles.forEach(r -> System.out.println(r.getName()));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
